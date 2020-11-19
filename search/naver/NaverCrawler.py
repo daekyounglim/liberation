e60f0240fdef6b160cb86c4159cac5878f705e2d
lim.d.1 <lim.d.1@pg.com>
2020-11-19 14:14:34 +0900

    Fix pypath and some bug fix

--- search/naver/NaverCrawler.py
+++ search/naver/NaverCrawler.py
@@ -3,133 +3,95 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import pandas as pd
 from ExcelFileReader import ExcelFileReader
-from util.util import Find
+import math
+from NaverCrawlerUrl import NaverCrawlerUrl
+from NaverCrawlerCurl import NaverCrawlerCurl
+from NaverScreenshot import open_chrome_driver, save_fullpage_screenshot
+from sqlalchemy import create_engine
+import sys
+import os
+
+sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath('../..'))
 
 
 class NaverCrawler:
+    def isNaN(self, string):
+        return string != string
 
-    def crawl_search_page(url=None):
-        '''Search Page Crawling by keyword'''
-
-        try:
-            html = requests.get(url, headers={'User-Agent': 'Moziilla/5.0'})
-            return html.content
-        except Exception as e:
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            traceback_details = {
-                     'filename': exc_traceback.tb_frame.f_code.co_filename,
-                     'lineno'  : exc_traceback.tb_lineno,
-                     'name'    : exc_traceback.tb_frame.f_code.co_name,
-                     'type'    : exc_type.__name__,
-                     'message' : str(e)
-                    }
-            print(traceback_details)
-            return ''
-
-    def parse_search_page(self, html_content, pid = 0):
-        '''Parse Html Page, and extract key data'''
-
-        mall_list = []
-        product_list = []
-        price_list = []
+    def transform_template(self):
+        data = ExcelFileReader.load('../data/naver_url.pkl')
         url_list = []
-        delivery_list = []
+        for row in data.values:
+            pid = row[0]
+            barcode = row[3]  # P&G Product barcode
+            #P&G product
+            if not self.isNaN(row[12]):
+                ncurl_pg = row[12]  # P&G Product curl url
+                url_list.append(['curl', 'PG', pid, barcode, ncurl_pg])
+            elif not self.isNaN(row[11]):
+                nurl_pg = row[11]  # P&G Product url url
+                url_list.append(['url', 'PG', pid, barcode, nurl_pg])                
+            
+            #Competitor 1
+            if not self.isNaN(row[32]):
+                ncurl_c1 = row[32]  # Competitor1 Product curl url
+                url_list.append(['curl', 'C1', pid, barcode, ncurl_c1])
+            elif not self.isNaN(row[31]):
+                nurl_c1 = row[31]  # Competitor1 Product url url
+                url_list.append(['url', 'C1', pid, barcode, nurl_c1])
+            
+            #Competitor 2
+            if not self.isNaN(row[50]):
+                ncurl_c2 = row[50]  # Competitor2 Product curl url
+                url_list.append(['curl', 'C2', pid, barcode, ncurl_c2])
+            elif not self.isNaN(row[49]):
+                nurl_c2 = row[49]  # Competitor2 Product url url
+                url_list.append(['url', 'C2', pid, barcode, nurl_c2])  
+        url_df = pd.DataFrame(url_list)
+        url_df.columns = ['urlType', 'infoType', 'pid','barcode', 'url']
+        return url_df
 
-        try:
-            soup_obj = BeautifulSoup(html_content, "html.parser")
-            tables = soup_obj.find_all("table", {"class": "tbl tbl_v"})
-            for table in tables:
-                #1 row in 1 table
-                tr = table.find("tr")
-                print("=======================================================================")
-                #find td tags, mall info
-                mall_list.append(self._parse_mall_info(tr))
-                #Get product name
-                product_list.append(self._parse_product_info(tr))
-                #Get price info
-                price_list.append(self._parse_price_info(tr))
-                #Get price url info
-                url_list.append(self._parse_price_url_info(tr))
-                #Get delivery cost info
-                delivery_list.append(self._parse_delivery_price_info(tr))
-
-        except Exception as e:
-            print(e)
-        
-        naver_df = pd.DataFrame(
-            {
-                'pid' : pid,
-                'mall': mall_list,
-                'product_name' : product_list,
-                'price': price_list,
-                'url': url_list,
-                'delivery_cost': delivery_list
-            }
-
-        )
-
-        return naver_df
-
-    def _parse_mall_info(tr):
-        try:
-            mall_td = tr.find_all("td")[0]
-            return mall_td.find('a')['data-mall-name']
-        except:
-            return ''
-
-    def _parse_product_info(tr):
-        try:
-            td_lft = tr.find("td", {"class": "lft"})
-            return td_lft.find('a').text
-        except:
-            return ''
-
-    def _parse_price_info(tr):
-        try:
-            return tr.find("td", {"class": "td_price"}).find('span').text.replace(',', '')
-        except:
-            return ''
-    
-    def _parse_price_url_info(tr):
-        try:
-            return tr.find("td", {"class": "td_price"}).find('a')['href']
-        except:
-            return ''
-    
-    def _parse_delivery_price_info(tr):
-        try:
-            delivery_td = tr.find_all("td")[3]
-            return delivery_td.find('p').text.replace('원', '').replace('무료배송', '0').replace(',', '')
-        except:
-            return ''
-
-    def run_craweler(self, url, result_df, info_type):
-        '''Check input is vaild url and extract key data'''
-
-        if "https://search.shopping.naver.com/" in str(url):
-            html = self.crawl_search_page(Find(url)[0])
-            naver_df = self.parse_search_page(self, html, pid)
-            naver_df['type'] = info_type
-            result_df = pd.concat([result_df, naver_df], axis = 0,  ignore_index=True)
-        return result_df
-
+    def insert_db(self, result_df):
+        print('====================================Insert DB==============================================')
+        print(result_df.sample(5))
+        print(result_df.columns)
+        engine = create_engine('postgresql+psycopg2://liberation:qwer1234@143.40.147.92:5432/liberation_db', echo=False)
+        result_df.set_index(['crawling_date', 'crawling_hour', 'barcode', 'nvMid'])
+        result_df.to_sql('naver_price', con=engine, if_exists='append')
 
 
 if __name__ == '__main__':
-    data = ExcelFileReader.load('../data/naver_url.pkl')
-    result_df = pd.DataFrame()
+    print('Naver Crawler Start')
+    d = open_chrome_driver()
+    crawler = NaverCrawler()
+    crawlerUrl = NaverCrawlerUrl() 
+    crawlerCurl = NaverCrawlerCurl() 
 
-    crawler = NaverCrawler
-    for row in data.values:
-        pid= row[0]
-        nurl_pg = row[11] #P&G Product url
-        nurl_1 = row[30] #Competitor 1 url
-        nurl_2 = row[47] #Competitor 2 url
+    url_df = NaverCrawler.transform_template(crawler)
+    print(url_df)
+    
+    result_df = pd.DataFrame()   
 
-        result_df = NaverCrawler.run_craweler(crawler, nurl_pg, result_df, 'P&G Product')
-        result_df = NaverCrawler.run_craweler(crawler, nurl_1, result_df, 'Competitor 1')
-        result_df = NaverCrawler.run_craweler(crawler, nurl_2, result_df, 'Competitor 2')
+    for i in url_df.index:
+        url_type = url_df._get_value(i, 'urlType')
+        info_type = url_df._get_value(i, 'infoType')
+        pid = url_df._get_value(i, 'pid')
+        url = url_df._get_value(i, 'url')
 
+        if url_type == 'url':
+            result_df = NaverCrawlerUrl.run_craweler(crawlerUrl, pid, url, result_df, info_type, d)
+        else:
+            result_df = NaverCrawlerCurl.run_craweler(crawlerCurl, pid, url, result_df, info_type, d)
+    
     print(result_df)
+
+    main_df = pd.merge(url_df, result_df, how='right', left_on = 'pid', right_on= 'pid').drop(['urlType', 'url'], axis=1)
+    #main_df.to_csv('result.csv',encoding='utf-8-sig')
+
+    crawler.insert_db(main_df)
+    d.close()
+
 
     
--- search/naver/NaverCrawlerCurl.py
+++ search/naver/NaverCrawlerCurl.py
@@ -2,10 +2,18 @@ import sys
 import os
 import json
 import subprocess
+import pandas as pd
+from datetime import datetime
+from sqlalchemy import create_engine
+import psycopg2
 
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('../..'))))
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('..'))))
+sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath('../..'))
+
 from ExcelFileReader import ExcelFileReader
+from util.util import findUrl
+from NaverScreenshot import open_chrome_driver, save_fullpage_screenshot
 
 
 class NaverCrawlerCurl:
@@ -13,7 +21,10 @@ class NaverCrawlerCurl:
     def _get_json_data_(self, curl_command):
         curl_command = curl_command.replace("\n", "")
         curl_command = curl_command.replace("\\", "")
-        result = subprocess.check_output(curl_command, shell=True)
+        curl_command = curl_command.replace("\'", "\"")
+        curl_command = curl_command.replace("--compressed", "")
+        #print(curl_command)
+        result = subprocess.check_output(f"{curl_command}", shell=True)
         result = result.decode('utf-8')
         json_obj = json.loads(result)
         return json_obj
@@ -23,28 +34,79 @@ class NaverCrawlerCurl:
         curl_list = []
         for row in data.values:
             pid = row[0]
+            barcode = row[3]  # P&G Product barcode
             ncurl_pg = row[12]  # P&G Product curl url
-            curl_list.append([pid, ncurl_pg])
+            curl_list.append([pid, barcode, ncurl_pg])
         return curl_list
 
-    def __parse_json__obj__(self, json_obj):
+    def __parse_json__obj__(self, json_obj, pid):
         """
         To-do : Json object를 파싱해서 dataframe로 만든다.
         """
+        try:
+            naver_df = pd.DataFrame({})
 
-    def __insert_data__(self, dataframee):
+            #json -> result -> products
+            result = self._parse_element(json_obj, 'result')
+            products = self._parse_element(result, 'products')
+                              
+            #for loop products list, 1 product level
+            rank = 0
+            while rank < len(products):                            
+                product_df = pd.DataFrame(products[rank], index=[0])
+                #filter columns
+                product_df = product_df[['nvMid', 'productName', 'mallName', 'channelName', 'pcPrice', 'mobilePrice', 'deliveryFee', 'pcProductUrl']]
+                #add rank of the product from the main list
+                product_df['pcRank'] = rank + 1
+                naver_df = pd.concat([naver_df, product_df], axis=0, ignore_index=True)
+                rank += 1
 
-        """
-        To-do : dataframe 을 테이블에 저장한다.
-        """
+            naver_df['pid'] = pid
+            naver_df['crawling_datetime'] = datetime.now()
+            naver_df['crawling_date'] = pd.to_datetime(naver_df['crawling_datetime'] ).dt.date
+            naver_df['crawling_hour'] = pd.to_datetime(naver_df['crawling_datetime'] ).dt.hour
+            naver_df = naver_df.astype({"pcRank": int})
+
+            return naver_df
+
+        except ValueError as ve:
+            print(ve)
+
+    def _parse_element(self, obj, element_name):
+        '''Parse obj by element'''
+        if obj is not None and obj[element_name] is not None:
+            return obj[element_name]
+        else:
+            #raise ValueError(f'Not Exist Error {element_name}')
+            return None
 
     def process(self):
         curl_list = self._read_template_()
-        for pid, curl in curl_list[:1]:  #excel template이 완성되면 제거할것.
+        for pid, barcode, curl in curl_list[:1]:  #excel template이 완성되면 제거할것.
             json = self._get_json_data_(curl)
             print(json)
-            #df = self.__parse_json__obj__(json)
-            #self.__insert_data__(df)
+            curl_df = self.__parse_json__obj__(json, pid)
+            curl_df['Barcode'] = barcode
+            print(curl_df)
+            print(curl_df.columns)
+
+    def run_craweler(self, pid, curl, result_df, info_type, d):
+        '''Check input is vaild url and extract key data'''
+
+        if "https://search.shopping.naver.com/" in str(curl):
+            #get page source
+            json = self._get_json_data_(curl)
+            #parse page source
+            naver_df = self.__parse_json__obj__(json, pid)
+            #pid
+            naver_df['pid'] = pid
+            #extract url
+            url_found= findUrl(curl)[0]
+            # take a screenshot of it and add file name to screenshot col
+            naver_df['screenshot'] = save_fullpage_screenshot(d, str(pid)+"_"+str(info_type), url_found)
+            #concatenate crawled procudt info
+            result_df = pd.concat([result_df, naver_df], axis = 0,  ignore_index=True)
+        return result_df
 
 if __name__ == '__main__':
     naver = NaverCrawlerCurl()
