8ca8c4b56e52d20b99a17b137678314357547b7a
lim.d.1 <lim.d.1@pg.com>
2020-11-19 14:16:54 +0900

    update shell command

--- search/run_liberation.sh
+++ search/run_liberation.sh
@@ -2,5 +2,5 @@
 /root/miniconda3/etc/profile.d/conda.sh
 cd /root/liberation/search
 echo $PWD
-/root/miniconda3/bin/python naver/NaverCrawlerJson.py > application.log
+/root/miniconda3/bin/python naver/NaverCrawler.py > application.log
 
